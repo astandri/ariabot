e6c64211b3cd9be7da4d7b0924165a0eac1929d6
Astandri Koesriputranto <astandri.k@ITID000520-MAC.local>
2019-04-03 19:06:35 +0700

    Created basic function Hello World

---
+++ credential.py
@@ -0,0 +1,20 @@
+CONSUMER_KEY = 'B2hNHqIAlIkkcJbC7luwjwi7h'
+CONSUMER_SECRET = 'TPnBk3ltbvjl4dAaKtq5EklotlEQFQWlTDa7fbEvFITWmkPXG9'
+ACCESS_KEY = '1112913758546931712-qUVAbNYcHgKJC0wItYBsIj1dq0aS9F'
+ACCESS_SECRET = 'RmVsJGlaK2TDZBULhVq7Bm9KFgG6zNRmlpcegC4wOW6Bc'
+
+class Credential():
+    def __init__(self):
+        self.type = 'Connector'
+
+    def getAccessKey():
+        return ACCESS_KEY
+
+    def getAccessSecret():
+        return ACCESS_SECRET
+
+    def getConsumerKey():
+        return CONSUMER_KEY
+
+    def getConsumerSecret():
+        return CONSUMER_SECRET
